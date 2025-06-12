77163348c1e5d46562b5bd5c7ba85582069ee5bd
Polly L-G <plinkoglegabriel@gmail.com>
2025-06-12 17:00:32 +0100

    Removing edge to edge and default styling so that themeing can be implemented. View binding setup has also been added

--- app/src/main/java/co/uk/bbk/ladlelibrary/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/ladlelibrary/MainActivity.kt
@@ -1,36 +1,24 @@
 package co.uk.bbk.ladlelibrary
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import android.widget.Button
-
-import androidx.core.view.WindowInsetsCompat
+import co.uk.bbk.ladlelibrary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recipeListButton : Button
-    private lateinit var addButton : Button
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        addButton = findViewById(R.id.btnToAddRecipe)
-        recipeListButton = findViewById(R.id.btnToRecipeList)
-
-        addButton.setOnClickListener {
+        binding.addButtonMain.setOnClickListener {
 //            implement
         }
 
-        recipeListButton.setOnClickListener {
+        binding.recipeListButtonMain.setOnClickListener {
 //            implement
         }
     }
