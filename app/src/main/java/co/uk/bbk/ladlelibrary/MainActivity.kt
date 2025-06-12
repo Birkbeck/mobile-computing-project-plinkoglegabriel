6b861e5b1c2ec3fc7e71e280887d0911f21708ef
Polly L-G <plinkoglegabriel@gmail.com>
2025-06-12 15:28:33 +0100

    Adding click listeners for buttons in main activity (homepage)

--- app/src/main/java/co/uk/bbk/ladlelibrary/MainActivity.kt
+++ app/src/main/java/co/uk/bbk/ladlelibrary/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import android.widget.Button
+
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recipeListButton : Button
+    private lateinit var addButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        addButton = findViewById(R.id.btnToAddRecipe)
+        recipeListButton = findViewById(R.id.btnToRecipeList)
+
+        addButton.setOnClickListener {
+//            implement
+        }
+
+        recipeListButton.setOnClickListener {
+//            implement
         }
     }
 }
