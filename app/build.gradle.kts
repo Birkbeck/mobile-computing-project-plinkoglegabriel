ea70dc99917d71c23f02494cde4f122dc9d7c8a1
Polly L-G <plinkoglegabriel@gmail.com>
2025-06-27 12:43:22 +0100

    Updating the room dependencies to work with the version of kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,11 +60,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.activity:activity-ktx:1.8.2")
 
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-
+    implementation("androidx.room:room-runtime:2.7.0-alpha01")
+    implementation("androidx.room:room-ktx:2.7.0-alpha01")
+    kapt("androidx.room:room-compiler:2.7.0-alpha01")
 
 }
