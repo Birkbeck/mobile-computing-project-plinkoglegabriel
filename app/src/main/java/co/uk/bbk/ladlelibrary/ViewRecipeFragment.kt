c7f20cb5a3ea23b92960f9a7fed45e66aea8761c
Polly L-G <plinkoglegabriel@gmail.com>
2025-06-28 10:50:38 +0100

    Updating ViewRecipeFragment to handle business logic in the MainViewModel and bind the text to the ui so that updates to the the recipe data are displayed

--- app/src/main/java/co/uk/bbk/ladlelibrary/ViewRecipeFragment.kt
+++ app/src/main/java/co/uk/bbk/ladlelibrary/ViewRecipeFragment.kt
@@ -7,14 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import co.uk.bbk.ladlelibrary.databinding.FragmentViewRecipeBinding
 import androidx.fragment.app.viewModels
-import co.uk.bbk.ladlelibrary.ViewRecipeViewModel
+import co.uk.bbk.ladlelibrary.MainViewModel
 
 class ViewRecipeFragment : Fragment() {
 
     private lateinit var binding: FragmentViewRecipeBinding
-    private val viewModel: ViewRecipeViewModel by viewModels()
+    private val viewModel: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,7 +33,6 @@ class ViewRecipeFragment : Fragment() {
         val args = arguments
         args?.let {
             val recipe = RecipeItem(
-                id = args.getInt("recipeId", 0),
                 imageResId = args.getInt("imageResId", 0),
                 title = args.getString("title") ?: "No Title",
                 shortDescription = args.getString("description") ?: "No Description",
@@ -41,8 +41,15 @@ class ViewRecipeFragment : Fragment() {
                 category = args.getString("category") ?: "No Category"
             )
             Log.i("BBK-LOG", "Recipe details received for: ${recipe.title}")
-            viewModel.setRecipe(recipe)
+            // recipe details bound to the UI
+            binding.recipeTitle.text = recipe.title
+            binding.recipeDescription.text = recipe.shortDescription
+            binding.ingredients.text = recipe.ingredients
+            binding.instructions.text = recipe.instructions
+            binding.category.text = recipe.category
+            binding.recipePhotoView.setImageResource(recipe.imageResId)
         }
+
 
     }
 
