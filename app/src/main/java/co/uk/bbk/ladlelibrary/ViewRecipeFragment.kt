4b4f8d22a79692e5c23dee15ecfc3b59327ad66b
Polly L-G <plinkoglegabriel@gmail.com>
2025-06-29 18:19:00 +0100

    Ensuring the recipe's id is parsed in and adding setOnClickListeners to the edit and delete buttons in the ViewRecipeFragment

--- app/src/main/java/co/uk/bbk/ladlelibrary/ViewRecipeFragment.kt
+++ app/src/main/java/co/uk/bbk/ladlelibrary/ViewRecipeFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import co.uk.bbk.ladlelibrary.databinding.FragmentViewRecipeBinding
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import co.uk.bbk.ladlelibrary.MainViewModel
 
 class ViewRecipeFragment : Fragment() {
@@ -33,6 +34,7 @@ class ViewRecipeFragment : Fragment() {
         val args = arguments
         args?.let {
             val recipe = RecipeItem(
+                id = args.getLong("id", 0L),
                 image = args.getString("image", R.drawable.placeholder_photo.toString()),
                 title = args.getString("title") ?: "No Title",
                 shortDescription = args.getString("description") ?: "No Description",
@@ -54,9 +56,33 @@ class ViewRecipeFragment : Fragment() {
                 Log.e("BBK-LOG", "Error parsing image URI: ${e.message}")
                 binding.recipePhotoView.setImageResource(R.drawable.placeholder_photo)
             }
+            binding.editButton.setOnClickListener {
+                Log.i("BBK-LOG","Edit recipe button clicked")
+                val bundle = Bundle().apply {
+                    putLong("id", recipe.id)
+                    putString("title", recipe.title)
+                    putString("image", recipe.image)
+                    putString("description", recipe.shortDescription)
+                    putString("ingredients", recipe.ingredients)
+                    putString("instructions", recipe.instructions)
+                    putString("category", recipe.category)
+                }
+                requireActivity().supportFragmentManager.setFragmentResult("edit_recipe", bundle)
+                findNavController().navigate(R.id.editRecipeFragment2, bundle)
+            }
+
+            binding.deleteButtonView.setOnClickListener {
+                Log.i("BBK-LOG","Delete recipe button clicked (ViewRecipeFragment)")
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Are you sure you want to delete this " + recipe.title + " recipe?")
+                    .setPositiveButton("Yes") { dialog, _ ->
+                        viewModel.deleteRecipe(recipe)
+                        findNavController().popBackStack()
+                    }
+                    .setNegativeButton("No", null)
+                    .show()
+            }
         }
-
-
     }
 
     companion object {
