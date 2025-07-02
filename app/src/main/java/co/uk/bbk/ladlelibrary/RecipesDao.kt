86966ce867d5279f638fa177b670bfb375027c75
Polly L-G <plinkoglegabriel@gmail.com>
2025-07-02 10:14:00 +0100

    Updating comments for the database and dao

--- app/src/main/java/co/uk/bbk/ladlelibrary/RecipesDao.kt
+++ app/src/main/java/co/uk/bbk/ladlelibrary/RecipesDao.kt
@@ -6,12 +6,14 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 
-
+// The RecipesDao interface is used to access the database and the RecipeItem entity
 @Dao
 interface RecipesDao {
+    // geting all recipes from the database
     @Query("SELECT * FROM Recipes;")
     suspend fun getAllRecipes(): List<RecipeItem>
 
+    // inserting, updating, deleting a recipe
     @Insert
     suspend fun insertRecipe(recipe: RecipeItem)
 
@@ -21,12 +23,15 @@ interface RecipesDao {
     @Delete
     suspend fun deleteRecipe(recipe: RecipeItem)
 
+    // getting a recipe by its id
     @Query("SELECT * FROM Recipes WHERE id = :id")
     suspend fun getRecipe(id: Long): RecipeItem?
 
+    // getting a recipe by its title to check there are no duplicates
     @Query("SELECT * FROM Recipes WHERE title = :title LIMIT 1")
     suspend fun getTitle(title: String): RecipeItem?
 
+    // getting a recipe by its title and id to check there are no duplicates when editing a recipe
     @Query("SELECT * FROM Recipes WHERE title = :title AND id != :id LIMIT 1")
     suspend fun getTitleWithId(title: String, id: Long): RecipeItem?
 }
--- app/src/main/java/co/uk/bbk/ladlelibrary/RecipesDatabase.kt
+++ app/src/main/java/co/uk/bbk/ladlelibrary/RecipesDatabase.kt
@@ -6,11 +6,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+
+// This is the database class for the Recipes app (extends RoomDatabase and allows access to the database)
 @Database(entities = [RecipeItem::class], version = 1)
 abstract class RecipesDatabase : RoomDatabase() {
-
+    // This is the abstract function that returns the RecipesDao interface
     abstract fun recipeDao(): RecipesDao
 
+    // creating an instance of the database
     companion object {
         @Volatile
         private var INSTANCE: RecipesDatabase? = null
