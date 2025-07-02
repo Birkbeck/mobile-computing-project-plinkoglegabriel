7b8ba867b87f50c5ca1c03e457db516773f44ea3
Polly L-G <plinkoglegabriel@gmail.com>
2025-07-02 10:15:09 +0100

    Updating comments for the ViewRecipeFragment

--- app/src/main/java/co/uk/bbk/ladlelibrary/ViewRecipeFragment.kt
+++ app/src/main/java/co/uk/bbk/ladlelibrary/ViewRecipeFragment.kt
@@ -14,27 +14,37 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import co.uk.bbk.ladlelibrary.MainViewModel
 
+// ViewRecipeFragment that allows users to view the details of each recipe
 class ViewRecipeFragment : Fragment() {
-
+    // Binding to the layout for this fragment
     private lateinit var binding: FragmentViewRecipeBinding
+    // Shared MainViewModel to access the data and business logic
     private val viewModel: MainViewModel by activityViewModels()
+
     private var recipeId: Long = 0L
 
+    // onCreateView method from the Fragment class that inflates the layout
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // view is bound to the FragmentViewRecipeBinding class (fragment_view_recipe.xml)
         binding = FragmentViewRecipeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    // onViewCreated method from the Fragment class used to set up the ui, observe the ViewModel, and handle click events
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // setting the title of the activity to "View Recipe"
         (activity as? MainActivity)?.setTitle("View Recipe")
         binding.lifecycleOwner = this
+        // assinging the recipeid variable to the id passed in the arguments bundle
+        // this is used to fetch the recipe details from the ViewModel
         recipeId = arguments?.getLong("id", 0L) ?: 0L
         viewModel.getRecipe(recipeId)
 
+        // observing the viewingRecipe LiveData value from the ViewModel to get the recipe details
         viewModel.viewingRecipe.observe(viewLifecycleOwner) { recipe ->
             recipe?.let {
                 Log.i("BBK-LOG", "Recipe details received for: ${recipe.title}")
@@ -44,6 +54,7 @@ class ViewRecipeFragment : Fragment() {
                 binding.ingredients.text = recipe.ingredients
                 binding.instructions.text = recipe.instructions
                 binding.category.text = recipe.category
+                // setting the image for the recipe (handling null or erroneous image URLs)
                 if (recipe.image.isNullOrBlank()) {
                     binding.recipePhotoView.setImageResource(R.drawable.placeholder_photo)
                 } else {
@@ -55,6 +66,7 @@ class ViewRecipeFragment : Fragment() {
                         binding.recipePhotoView.setImageResource(R.drawable.placeholder_photo)
                     }
                 }
+                // setting the click listeners for the edit button, getting the recipe details and passing them to the EditRecipeFragment (which is then navigated to)
                 binding.editButton.setOnClickListener {
                     Log.i("BBK-LOG", "Edit recipe button clicked")
                     val bundle = Bundle().apply {
@@ -73,6 +85,7 @@ class ViewRecipeFragment : Fragment() {
                     findNavController().navigate(R.id.editRecipeFragment2, bundle)
                 }
 
+                // setting the click listener for the delete button, showing an AlertDialog to confirm deletion of the recipe and navigating back to RecipeListFragment if confirmed
                 binding.deleteButtonView.setOnClickListener {
                     Log.i("BBK-LOG", "Delete recipe button clicked (ViewRecipeFragment)")
                     AlertDialog.Builder(requireContext())
@@ -87,11 +100,13 @@ class ViewRecipeFragment : Fragment() {
             }
         }
     }
+    // onResume method from the Fragment class that ensures that the recipe details in the ViewRecipeFragment are updated when the fragment is resumed after editing
     override fun onResume() {
         super.onResume()
         viewModel.getRecipe(recipeId)
     }
 
+    // Companion object to create a new instance of the ViewRecipeFragment with the recipe details passed as arguments
     companion object {
         @JvmStatic
         fun newInstance(
